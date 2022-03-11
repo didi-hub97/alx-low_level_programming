535b7bfe2a74836832a321738a1e766acc9d9c5a
didi-hub97 <elizabethgbamwuan@gmail.com>
2022-03-11 01:43:35 +0100

    print using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - program startup
+(*
+ * Description: prints the exact phrase followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }	
