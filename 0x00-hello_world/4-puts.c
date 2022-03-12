db80c2448b88ba0be9e891b23550cde6bd0d6b1d
didi-hub97 <email for my second account>
2022-03-13 00:57:31 +0100

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - program startup
+(*
+ * Description: prints the exact phrase followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+<<<<<<< HEAD
+  puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }	
+=======
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+>>>>>>> 1879f3d2dc69274aa0e9c4b062e507bb8da9892a
