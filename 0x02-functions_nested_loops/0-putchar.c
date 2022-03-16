a8ba61bd7a0e9bb80ca150850b6039b8d99d99b4
didi-hub97 <email for my second account>
2022-03-16 09:42:27 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
-#include "main.h"
+# include "main.h"
+
 /**
- * main -  prints _purtchar, followed by a new line
- * Return: Always 0 (Success)
+ *main - print out main car by char.
+ *Description: uses the main header file
+ *that contains the function _putchar
+ *Return: 0.
  */
+
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+char str[] = "_putchar";
+int i;
 
-	return (0);
+i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+++i;
+}
+_putchar('\n');
+
+return (0);
 }
