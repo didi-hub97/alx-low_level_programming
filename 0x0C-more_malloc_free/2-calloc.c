90ba3a43b04b4ccf86e82400857475cfc00496e6
didi-hub97 <email for my second account>
2022-04-08 05:20:51 +0100

    Allocate memory space

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,22 +6,38 @@
  * @size: input string.
  * Return: Pointer.
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *p;
-unsigned int i, d;
+	char *ptr;
 
-d = nmemb * size;
-if (nmemb <= 0 || size <= 0)
-return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-p = malloc(d);
-if (p == NULL)
-return (NULL);
+	ptr = malloc(size * nmemb);
 
-for (i = 0; i < (d); i++)
-{
-p[i] = 0;
-}
-return (p);
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
