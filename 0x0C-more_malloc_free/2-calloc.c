872f5db039ef7f5ac2f74a630687cceb70400990
didi-hub97 <email for my second account>
2022-04-08 05:02:40 +0100

    Allocate memory space

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - allocates memory for an array.
- * @nmemb: number of elements.
- * @size: size of bytes.
- *
- * Return: pointer to the allocated memory.
- * if nmemb or size is 0, returns NULL.
- * if malloc fails, returns NULL.
+ * _calloc - prints calloc;
+ * @nmemb: input string.
+ * @size: input string.
+ * Return: Pointer.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+char *p;
+unsigned int i, d;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+d = nmemb * size;
+if (nmemb <= 0 || size <= 0)
+return (NULL);
 
-	p = malloc(nmemb * size);
+p = malloc(d);
+if (p == NULL)
+return (NULL);
 
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
-
-	return (p);
+for (i = 0; i < (d); i++)
+{
+p[i] = 0;
+}
+return (p);
 }
